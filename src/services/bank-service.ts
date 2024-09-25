b9d652d3c48275e6c380c15ae01d0a8da6a27d60
revanthkumarJ <jrevanth101@gmail.com>
2024-09-25 11:46:08 +0530

    updated some due routes

--- src/services/bank-service.ts
+++ src/services/bank-service.ts
@@ -4,12 +4,13 @@ import bankRepository from "../repository/bank-repository"
 const bankServices = {
   async getBankDueDetails(ReceiptNo: String) {
     try {
-      const due = await bankRepository.getBankDueDetails(ReceiptNo);
-      console.log(due);
-      if (!due) {
+      const tutionFee = await bankRepository.getBankTutionDueDetails(ReceiptNo);
+      const hostelFee = await bankRepository.getBankHostelDueDetails(ReceiptNo);
+      
+      if (!(tutionFee || hostelFee)) {
         return { status: 404, message: "Due No. not found" };
       }
-      return { status: 200, due };
+      return { status: 200, tutionFee,hostelFee };
     } catch (error) {
       throw error;
     }
